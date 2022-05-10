d21484443fb0b93f843421db117cd884a16991c3
MehruzSaif <mehruz.saif@g.bracu.ac.bd>
2022-05-10 21:25:57 +0600

    added some component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
 import Navbar from './Pages/Shared/Navbar/Navbar';
+import { Routes, Route, Link } from "react-router-dom";
+import Home from './Pages/Home/Home';
+import About from './Pages/About/About';
 
 function App() {
   return (
     <div>
       <Navbar></Navbar>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+      </Routes>
     </div>
   );
 }
