8fbb6c3ae58de8715619f0bdb78d8389929b8c99
MehruzSaif <mehruz.saif@g.bracu.ac.bd>
2022-05-11 20:50:39 +0600

    services component added

---
+++ src/Pages/Home/Services.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Services = () => {
+    return (
+        <div className='my-20'>
+            <div className='text-center'>
+                <h3 className='text-primary text-xl font-bold uppercase'>Our Services</h3>
+                <h2 className='text-4xl'>Services we provide</h2>
+            </div>
+
+            <div>
+
+            </div>
+            
+        </div>
+    );
+};
+
+export default Services;
