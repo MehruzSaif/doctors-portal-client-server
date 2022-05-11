f2deb9ebd6ec07b73661141b99b98188dba939a0
MehruzSaif <mehruz.saif@g.bracu.ac.bd>
2022-05-12 00:16:25 +0600

    MakeAppointment component resize

--- src/Pages/Home/MakeAppointment.js
+++ src/Pages/Home/MakeAppointment.js
@@ -1,17 +1,27 @@
 import React from 'react';
 import doctor from '../../assets/images/doctor.png';
+import appointment from '../../assets/images/appointment.png';
+import PrimaryButton from '../Shared/PrimaryButton/PrimaryButton';
 
 const MakeAppointment = () => {
     return (
-        <section>
-            <div>
+        <section style={{
+            background: `url(${appointment})`
+        }} 
+        
+        className='flex justify-center items-center'>
+            <div className='flex-1'>
                 <img src={doctor} alt="" />
             </div>
 
-            <div>
+            <div className='flex-1'>
                 <h3 className='text-xl text-primary'>Appointment</h3>
-                <h2>Make an Appointment Today</h2>
+                <h2 className='text-3xl text-white'>Make an Appointment Today</h2>
+                <p className='text-white'>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsumis that it has a more-or-less normal distribution of letters,as opposed to using 'Content here, content here', making it look like.</p>
+
+            <PrimaryButton>GET STARTED</PrimaryButton>
             </div>
+
         </section>
     );
 };
--- src/Pages/Home/Services.js
+++ src/Pages/Home/Services.js
@@ -6,6 +6,7 @@ import Service from './Service';
 import treatment from '../../assets/images/treatment.png';
 
 import './Services.css';
+import PrimaryButton from '../Shared/PrimaryButton/PrimaryButton';
 
 
 const Services = () => {
@@ -52,7 +53,7 @@ const Services = () => {
                         <h1 className="text-5xl font-bold">Exceptional Dental Care, on Your Terms</h1>
                         <p className="py-6">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsumis that it has a more-or-less normal distribution of letters,as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page.</p>
                         
-                        <button className="btn btn-primary uppercase text-white font-bold bg-gradient-to-r from-secondary to-primary">Get Started</button>
+                        <PrimaryButton>GET STARTED</PrimaryButton>
                     </div>
                 </div>
             </div>
