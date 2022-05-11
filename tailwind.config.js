942ef627e2cbd890da93142800f402ee87213498
MehruzSaif <mehruz.saif@g.bracu.ac.bd>
2022-05-11 06:09:58 +0600

    banned img added

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,20 +3,20 @@ module.exports = {
   theme: {
     extend: {},
   },
-    daisyui: {
-      coctorthemes: [
-        {
-          mytheme: {
-            primary: "#a991f7",
-            secondary: "#f6d860",
-            accent: "#37cdbe",
-            neutral: "#3d4451",
-            "base-100": "#ffffff",
-          },
+  daisyui: {
+    themes: [
+      {
+        theme: {
+          primary: "#0FCFEC",
+          secondary: "#19D3AE",
+          accent: "#3A4256",
+          neutral: "#3d4451",
+          "base-100": "#ffffff",
         },
-        "dark",
-        "cupcake",
-      ],
-    },
+      },
+      "dark",
+      "cupcake",
+    ],
+  },
   plugins: [require("daisyui")],
 }
