9364ed41db20ee92b162600413ddc30d8759e550
MehruzSaif <mehruz.saif@g.bracu.ac.bd>
2022-05-13 04:14:56 +0600

    useState declare on AvailableAppo.js

--- src/Pages/Appointment/AppointmentBanner.js
+++ src/Pages/Appointment/AppointmentBanner.js
@@ -8,7 +8,7 @@ const AppointmentBanner = ({date, setDate}) => {
     return (
         <div class="hero min-h-screen">
             <div class="hero-content flex-col lg:flex-row-reverse">
-                <img src={chair} class="max-w-sm rounded-lg shadow-2xl" alt='Dentist Chair' />
+                <img src={chair} class="rounded-lg shadow-2xl max-w animated" alt='Dentist Chair' />
                 <div>
                     <DayPicker
                         mode="single"
