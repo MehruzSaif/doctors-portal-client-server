1c287faa53cb1470e60863cb4b6dd4cfcf12e330
MehruzSaif <mehruz.saif@g.bracu.ac.bd>
2022-05-13 06:05:20 +0600

    service.js component added

--- src/Pages/Appointment/AppointmentBanner.js
+++ src/Pages/Appointment/AppointmentBanner.js
@@ -1,12 +1,17 @@
 import chair from '../../assets/images/chair.png';
 import { DayPicker } from 'react-day-picker';
 import 'react-day-picker/dist/style.css';
+import bg from '../../assets/images/bg.png';
 
 const AppointmentBanner = ({date, setDate}) => {
 
 
     return (
-        <div class="hero min-h-screen">
+        <div className="hero min-h-screen"
+        style={{
+            background: `url(${bg})`
+        }}
+        class="hero min-h-screen">
             <div class="hero-content flex-col lg:flex-row-reverse">
                 <img src={chair} class="rounded-lg shadow-2xl max-w animated" alt='Dentist Chair' />
                 <div>
--- src/Pages/Appointment/Service.js
+++ src/Pages/Appointment/Service.js
@@ -2,15 +2,22 @@ import React from 'react';
 
 const service = ({ service }) => {
 
-    const {name, slots} = service;
+    const { name, slots } = service;
 
     return (
         <div class="card lg:max-w-lg bg-base-100 shadow-xl">
-            <div class="card-body">
-                <h2 class="card-title">{name}</h2>
+            <div class="card-body text-center my-5">
+                <h2 class="card-title text-secondary my-5">{name}</h2>
+                <p>
+                    {
+                        slots.length > 0
+                        ? <span>{slots[0]}</span>
+                        : <span className='text-red-500'>No Slot Available. Try Another day</span>
+                    }
+                </p>
                 <p>{slots.length} {slots.length > 1 ? 'spaces' : 'space'} available</p>
-                <div class="card-actions justify-end">
-                    <button class="btn btn-primary">Buy Now</button>
+                <div class="card-actions justify-center">
+                    <button disabled={slots.length === 0} class="btn btn-primary uppercase text-white font-bold bg-gradient-to-r from-secondary to-primary mt-5">Book Appointment</button>
                 </div>
             </div>
         </div>
