c8e27edc81148d878c691d06c624e5cb5c7314c7
MehruzSaif <mehruz.saif@g.bracu.ac.bd>
2022-05-15 06:10:41 +0600

    signout button added on navbar

--- src/Pages/Shared/Navbar/Navbar.js
+++ src/Pages/Shared/Navbar/Navbar.js
@@ -1,3 +1,4 @@
+import { signOut } from 'firebase/auth';
 import React from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { Link } from 'react-router-dom';
@@ -7,13 +8,17 @@ const Navbar = () => {
 
     const [user, loading, error] = useAuthState(auth);
 
+    const logout = () => {
+        signOut(auth);
+      };
+
     const menuItems = <>
         <li><Link to='/'>Home</Link></li>
         <li><Link to='/appointment'>Appointment</Link></li>
         <li><Link to='/review'>Review</Link></li>
         <li><Link to='/contact'>Contact</Link></li>
         <li><Link to='/about'>About</Link></li>
-        <li><Link to='/login'>Login</Link></li>
+        <li>{user ? <button class="btn btn-ghost" onClick={logout}>Sign Out</button> : <Link to='/login'>Login</Link>}</li>
     </>
 
     return (
