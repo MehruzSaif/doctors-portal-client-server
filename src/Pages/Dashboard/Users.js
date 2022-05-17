1d01e1d9c5e39882d3c223ad2704a70625544687
MehruzSaif <mehruz.saif@g.bracu.ac.bd>
2022-05-17 06:58:06 +0600

    All users length shown on dashboard and isLoading used

--- src/Pages/Dashboard/Users.js
+++ src/Pages/Dashboard/Users.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import { useQuery } from 'react-query';
+import Loading from '../Shared/Loading';
 
 const Users = () => {
+    const {data: users, isLoading} = useQuery('users', () => fetch('http://localhost:5000/user').then(res => res.json()));
+    if(isLoading) {
+        return <Loading></Loading>
+    }
+
     return (
         <div>
-            <h2 className='text-2xl'>All Users</h2>
+            <h2 className='text-2xl'>All Users: {users.length}</h2>
         </div>
     );
 };
