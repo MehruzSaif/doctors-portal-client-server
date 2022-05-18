7cd4227d112241b812def9817f17467bf3aa420c
MehruzSaif <mehruz.saif@g.bracu.ac.bd>
2022-05-18 20:39:21 +0600

    review section changed

--- src/Pages/Home/Testimonials.js
+++ src/Pages/Home/Testimonials.js
@@ -16,7 +16,7 @@ const Testimonials = () => {
         },
         {
             _id: 2,
-            name: 'Warisha Irish',
+            name: 'Jaanemaan',
             review: 'It is a long established fact that by the readable content of a lot layout. The point of using Lorem a more-or-less normal distribu to using Content here, content',
             location: 'New York',
             img: people2
