6c6e0a5205db706d44053aeda3b520d1424b8cb0
MehruzSaif <mehruz.saif@g.bracu.ac.bd>
2022-05-19 08:03:32 +0600

    isLoading implemnet to dave from undefined

--- src/Pages/Dashboard/ManageDoctors.js
+++ src/Pages/Dashboard/ManageDoctors.js
@@ -4,13 +4,13 @@ import Loading from '../Shared/Loading';
 
 const ManageDoctors = () => {
 
-    const { data: doctors, isLoading } = useQuery('doctors', () => fetch('', {
+    const { data: doctors, isLoading } = useQuery('doctors', () => fetch('http://localhost:5000/doctor', {
         headers: {
             authorization: `Bearer ${localStorage.getItem('accessToken')}`
         }
     }).then(res => res.json()))
 
-    if(isLoading) {
+    if (isLoading) {
         return <Loading></Loading>
     }
 
