34706d65261a2309207bd923f2158842f17b3d0d
MehruzSaif <mehruz.saif@g.bracu.ac.bd>
2022-05-19 07:53:07 +0600

    get the doctors from node.js and use useQuery for finding the doctors length

--- src/Pages/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard.js
@@ -26,7 +26,8 @@ const Dashboard = () => {
                     <li><Link to='/dashboard/history'>My History</Link></li>
                     {admin && <>
                         <li><Link to='/dashboard/users'>All Users</Link></li>
-                        <li><Link to='/dashboard/addDoctor'>Add Doctor</Link></li>
+                        <li><Link to='/dashboard/addDoctor'>Add a Doctor</Link></li>
+                        <li><Link to='/dashboard/manageDoctor'>Manage Doctors</Link></li>
                     </>}
                 </ul>
 
